d9a800842af04857a932653a98b71a7d3e4223c2
Parth Marfatia <parthmarfatia153@gmail.com>
2023-09-24 00:32:03 +0530

    middleware changes

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -2,10 +2,13 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
-  console.log(request.url);
+type Props = {
+  params: {
+    id: string;
+  };
+};
 
+export async function GET(request: Request, { params: { id } }: Props) {
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
   const todo: Todo = await res.json();
